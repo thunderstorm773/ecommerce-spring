0cd25b8060fb34f65bba6de19f3bf56e7090603f
Luko Delidzhov <luko193@gmail.com>
2025-02-27 21:52:51 +0200

    + Added additional configs for cors policy

--- ecommerce/src/main/java/com/tu/ecommerce/config/CORSConfig.java
+++ ecommerce/src/main/java/com/tu/ecommerce/config/CORSConfig.java
@@ -2,6 +2,8 @@ package com.tu.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -17,6 +19,9 @@ public class CORSConfig {
 
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(allowedOrigins);
+        config.setAllowCredentials(true);
+        config.setAllowedMethods(List.of(HttpMethod.GET.name(), HttpMethod.POST.name()));
+        config.setAllowedHeaders(List.of(HttpHeaders.ORIGIN, HttpHeaders.CONTENT_TYPE, HttpHeaders.ACCEPT));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
